c05b5e79ee5fc4e35ea9b47fde66689cdd57ee7b
Blake VandeMerwe <blakev@null.net>
2015-02-27 00:07:11 -0700

    changed the "default" article.html -- still in testing format

--- synpost/objects/content/article.py
+++ synpost/objects/content/article.py
@@ -1,9 +1,13 @@
 __author__ = 'Blake'
 
+from jinja2 import Template
+
 from basetypes import TextContentAsset
 from synpost.extensions import Category
 
 class Article(TextContentAsset):
+    theme_piece = 'article.html'
+
     def __init__(self, obj, site = None):
         super(Article, self).__init__(obj, site)
 
@@ -12,3 +16,10 @@ class Article(TextContentAsset):
     @property
     def extension(self):
         return 'html'
+
+    @property
+    def finalized_html(self):
+        Piece = self.site.theme.asset_by_filename('pieces', self.theme_piece)
+        template = self.site.theme.jinja_environment.get_template(Piece.filename)
+        template = template.render(rendered_content = self.as_HTML, **self.jinja_obj)
+        return template
